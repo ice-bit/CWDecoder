c96c8ba2d7ef8efdb4e9b50b96b85fb57ea1f77f
ice-bit <ceticamarco@gmail.com>
2020-05-30 01:39:12 +0200

    Added comments and improved mem usage

--- main.ino
+++ main.ino
@@ -1,11 +1,13 @@
+#include <string.h>
 #define BUZZER_PIN  A5 // Buzzer pin
-#define KEY_PIN  3 // Straight key pin
+#define KEY_PIN 3 // Straight key pin
 
-const uint8_t shortPress = 50;
-const uint8_t longPress = 200;
-const int wpm = 700;
-uint64_t timeLast = 0;
-String vec;
+const uint8_t shortPress = 50; // ms for a short press(dit)
+const uint8_t longPress = 200; // ms for a long press(dah)
+const uint16_t wpm = 700; // words-per-minutes
+uint64_t timeLast = 0; // ms since last word
+int16_t pos = -1; // default value means empty buffer
+char buf[256]; // A buffer to store data for conversion
 
 typedef struct Button {
   const uint8_t pin = KEY_PIN;
@@ -18,6 +20,9 @@ typedef struct Button {
 
 // create a new button instance
 Button_t key;
+
+// Internal function
+void buttonEvent(uint8_t type_of_event);
 
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
@@ -48,37 +53,46 @@ void loop() {
     } else if(key.currentState == LOW) {
       // Button is no longer pressed, get how long it was 
       // in previous state(ie how long it was pressed)
-      uint64_t currentMillis = millis();
+      uint64_t currentTime = millis();
       // If active key time is at least equal to short press time and lower than long press time 
       // then handle a short press event
-      if(((currentMillis - key.counter) >= shortPress) && (currentMillis - key.counter) < longPress)
-        shortPressEvent(); // Handle a short press
-      else if((currentMillis - key.counter) >= longPress) // Otherwise
-        longPressEvent(); // Handle a long press
+      if(((currentTime - key.counter) >= shortPress) && (currentTime - key.counter) < longPress)
+        buttonEvent(0); // Handle a short press
+      else if((currentTime - key.counter) >= longPress) // Otherwise
+        buttonEvent(1); // Handle a long press
       // Disable tone generator and turn dff the LED
       noTone(BUZZER_PIN);
       digitalWrite(LED_BUILTIN, LOW);
     }
     // Then update previous state for the next iteration
     key.prevState = key.currentState;
-    Serial.println(vec);
+    Serial.println(buf);
+  }
+
+  // Prevent buffer overflow
+  if(pos >= 255) {
+    memset(buf, 0, sizeof(buf));
+    pos = -1;
   }
 }
 
-void shortPressEvent() {
-  if(((millis() - timeLast) > wpm) && (vec != NULL)) {
-    vec += ' ';
-    Serial.println("spazio1");
+void buttonEvent(uint8_t type_of_event) {
+  if(type_of_event == 0) { // short press
+    /* if endTime-starTime (since last keystroke)
+      is greater than word-per-minutes time and 
+      buffer is empty, add a marker between words.
+      Buffer must not be empty to prevent adding 
+      marker char at the first input. */
+    if(((millis() - timeLast) > wpm) && (pos != -1))
+      buf[++pos] = ' ';
+
+    buf[++pos] = '.';
+    timeLast = millis(); // Update time since last word
+  } else if(type_of_event == 1) { // long press
+    if(((millis() - timeLast) > wpm) && (pos != -1))
+      buf[++pos] = ' ';
+
+    buf[++pos] = '-';
+    timeLast = millis(); // Update time since last word
   }
-
-  vec += '.';
-  timeLast = millis();
-}
-
-void longPressEvent() {
-  if(((millis() - timeLast) > wpm) && (vec != NULL))
-    vec += ' ';
-
-  vec += '-';
-  timeLast = millis();
 }
